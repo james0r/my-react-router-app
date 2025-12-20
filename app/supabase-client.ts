76ae370a2ad9ca61d23b58291ef14ae35eb51330
James <james.auble@gmail.com>
2025-12-19 16:06:25 -0800

    renamed netlify plugin var

--- app/supabase-client.ts
+++
@@ -1,6 +0,0 @@
-import {createClient} from '@supabase/supabase-js';
-
-// export const supabase = createClient(
-//     'https://wfwptmvyepbmmzichggb.supabase.co', 
-//     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indmd3B0bXZ5ZXBibW16aWNoZ2diIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxMDIzODgsImV4cCI6MjA4MTY3ODM4OH0.rZFX9rdxyTWFeYMsvXLCz0CdYXUP0NHaVdT1OFIvhf0'
-// );
