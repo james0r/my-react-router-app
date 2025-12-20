66fac203b0482d7e94b954457a8910be8fd53270
James <james.auble@gmail.com>
2025-12-19 16:13:08 -0800

    added netlify vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import netlifyReactRouter from '@netlify/vite-plugin-react-router'
+import netlify from '@netlify/vite-plugin'
 
 export default defineConfig({
   plugins: [
     tailwindcss(), 
     reactRouter(), 
     tsconfigPaths(),
-    netlifyReactRouter()
+    netlifyReactRouter(),
+    netlify()
   ],
 });
